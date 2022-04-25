7e521770165813179afb8e41813f84e7ba4df3d0
Serhii Demydenko <demydenko.serhii1994@gmail.com>
2022-04-25 17:51:15 +0300

    Update 02-timer.js

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -24,6 +24,7 @@ flatpickr('#datetime-picker', {
     newDate = selectedDates[0].getTime();
     if (newDate < Date.now()) {
       onSetAttributeDisabled(ref.btn);
+      Notify.failure('Please choose a date in the future');
     } else {
       onRemoveAttributeDisabled(ref.btn);
     }
